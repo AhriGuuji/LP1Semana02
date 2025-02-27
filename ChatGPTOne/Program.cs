0a9ff17a68cf31c94d64a875fa040ef4192ed5d3
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-27 16:52:37 +0000

    add ChatGPTOne

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            //Variaveis usadas;
+            string question;
+
+            //Pedir input;
+            Console.WriteLine("Ask me something");
+            question = "";
+
+            //Ciclo de perguntas e respostas
+            while (question != "EXIT")
+            {
+                question = Console.ReadLine();
+                    Console.WriteLine("Is about it");
+                    continue;
+                }
+            }
+        }
+    }
+}
