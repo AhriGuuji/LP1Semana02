c9762b5cd1af87d12237d2d9c752047dfa4b44e6
Lisa Carvalho <i.m.lisaaa14@gmail.com>
2025-02-27 09:56:03 +0000

    Add SmoothOperator

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Write a int number:");
+            string askedNumber = Console.ReadLine();
+            sbyte savedNumber = sbyte.Parse(askedNumber);
+            
+            Console.WriteLine($"Resultado da operação de decremento de {askedNumber} é: {--savedNumber}");
+            Console.WriteLine($"Resultado da operação de incremento de {askedNumber} é: {++savedNumber}");
+            
+        }
+    }
+}
